7797daef4fdfd2fcb7d9da3ed9675d76308b8bc3
EvanLidk <evanlidk@163.com>
2020-02-19 17:23:09 +0800

    a04

--- src/a04/SearchArray.java
+++ src/a04/SearchArray.java
@@ -5,8 +5,8 @@ package a04;
  * 题目:在一个二维数组中，每一行都按照从左到右递增的顺序排序，
  * 每一列都按照从上到下递增的顺序排序。
  * 请完成一个函数，输入这样的个二维数组和一个整数，判断数组中是否含有该整数。
- *
- *
+ * <p>
+ * <p>
  * 根据顺序找出一排一列，直接找到这排这列的交叉点为止
  */
 public class SearchArray {
@@ -15,49 +15,42 @@ public class SearchArray {
     public static void main(String[] args) {
         int[][] array = new int[][]{{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
 
-        System.out.println(find(18, array));
+        System.out.println(find(8, array));
 
 
     }
 
 
     private static boolean find(int target, int[][] array) {
-        int row = 0, line = 0;
+        int row = 0, line = array[0].length - 1;
 
+        while (true) {
+            int temp = array[row][line];
 
-        if (array[0][0] == target) {
-            return true;
-        }
-        while (line <= array[0].length - 1 && array[0][line] <= target) {
-            if (array[0][line] == target) {
+            if (temp > target) {
+                line--;
+                if (line < 0) {
+                    return false;
+                }
+            }
+
+            if (temp == target) {
                 return true;
             }
-            line++;
-        }
 
-        while (row <= array.length - 1 && array[row][0] <= target) {
-            if (array[row][0] == target) {
-                return true;
+
+            if (temp < target) {
+                row++;
+
+                if (row > array.length) {
+                    return false;
+                }
             }
-            row++;
+
+
+
         }
 
-        line--;
-        row--;
-
-        for (int i = 1; i <= row; i++) {
-            if (array[line][i] == target) {
-                return true;
-            }
-        }
-
-        for (int i = 1; i < line; i++) {
-            if (array[i][row] == target) {
-                return true;
-            }
-        }
-        return false;
 
     }
-
 }
